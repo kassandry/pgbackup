ad219d2763fe03fb0fc2ede13433c752d5a805f3
Lacey Powers <lacey.leanne@gmail.com>
2016-03-27 07:23:16 -0700

    Fixing a small bug with overzealous quoting. Adding checksums to each of the files for comparison on backups. Adding the ability to take weekly backups only. Adding the ability to designate the day that weekly backups are cleaned up. Bump license dates.

--- pgbackup.sh
+++ pgbackup.sh
@@ -10,20 +10,26 @@ function usage()
     echo "OPTIONS: "
     echo "-h Help (prints usage)"
     echo "-c configuration file location. Defaults to the cwd of pgbackup.sh with a file of pgbackup.config"
-    echo "-d Take a backup with a datestamp of %Y-%m-%d_%H:%M:%S_%Z"
+    echo "-d Take a backup with a datestamp of %Y-%m-%d_%H:%M:%S_%Z, or your own date(1) compatible format string"
 }
 
 DATESTAMP="$(date +%a)"
 CONFIG="$(pwd)/pgbackup.config"
 
-while getopts ":hdc:" OPTIONS; do
+while getopts ":hd:c:" OPTIONS; do
 	case ${OPTIONS} in
 	h)
 		usage
 		exit 1
 		;;
 	d)
-		DATESTAMP="$(date +%a_%F_%T_%Z)"
+		if [[ -z $OPTARG ]]; then
+			DATESTAMP="$(date +%a_%F_%T_%Z)"
+		else
+			# shellcheck disable=SC2086
+			# If this is invalid, date fails and exits
+			DATESTAMP="$(date $OPTARG)"
+		fi
 		;;
 	c)
 		CONFIG="$OPTARG"
@@ -58,10 +64,14 @@ if [[ $(uname -s ) == "Linux" ]]; then
 	# GNU Date
 	YESTERDAY=$(date --date="yesterday" "+%a")
 	LASTWEEK=$(date --date="last-week" "+week_%W_of_%Y")
+	MD5=$("$WHICH" md5sum)
+	MD5FLAGS="--tag"
 elif [[ $(uname -s) == "FreeBSD" ]]; then
 	# BSD Date
 	YESTERDAY=$(date -v "-1d" "+%a")
 	LASTWEEK=$(date -v "-1w" "+week_%W_of_%Y")
+	MD5=$("$WHICH" md5)
+	MD5FLAGS=""
 else 
 	echo "Unsupported OS Type"
 	exit 1
@@ -94,46 +104,68 @@ function check_all_success()
 	return 0
 }
 
-
 # $PGHOST is already double-quoted above. Additional double quoting changes the behavior
 # Suppressions for shellcheck added accordingly.
-
 # Back Up Each Database in compressed format.
-# shellcheck disable=SC2086
-for DATNAME in $($PSQL -U $PGUSER template1 $PGHOST -p $PGPORT --tuples-only -c "SELECT datname FROM pg_database WHERE datistemplate IS FALSE;"); do
+if [[ $WEEKLYONLY == false ]]; then 
 	# shellcheck disable=SC2086
-	 $PGDUMP -U $PGUSER $PGHOST -p $PGPORT "$DATNAME" "$PGDUMP_FLAGS" -f "${BACKUPDIR}/${SERVERNAME}_${DATNAME}_${PGPORT}_${DATESTAMP}.sqlc"
-	 RETVAL=$?
-	 STATUS=("${STATUS[@]}" "$RETVAL")
-done
-
-# Back Up The Globals.
-# shellcheck disable=SC2086 
-$PGDUMPALL -U $PGUSER $PGHOST -p $PGPORT "$PGDUMPALL_FLAGS" > "${BACKUPDIR}/${SERVERNAME}_globals_${PGPORT}_${DATESTAMP}.sql"
-RETVAL=$?
-STATUS=("${STATUS[@]}" "$RETVAL")
+	for DATNAME in $($PSQL -U $PGUSER template1 $PGHOST -p $PGPORT --tuples-only -c "SELECT datname FROM pg_database WHERE datistemplate IS FALSE;"); do
+		PGFILE="${BACKUPDIR}/${SERVERNAME}_${DATNAME}_${PGPORT}_${DATESTAMP}.sqlc"
+		# Clean up the previous checksum file
+		rm -vf "${PGFILE}.checksum"
+		# shellcheck disable=SC2086
+		$PGDUMP -U $PGUSER $PGHOST -p $PGPORT "$DATNAME" "$PGDUMP_FLAGS" -f "$PGFILE"
+		RETVAL=$?
+		STATUS=("${STATUS[@]}" "$RETVAL")
+		"$MD5" $MD5FLAGS "$PGFILE" >| "${PGFILE}.checksum"
+	done
+	# Back Up The Globals.
+	PGGLOBALS="${BACKUPDIR}/${SERVERNAME}_globals_${PGPORT}_${DATESTAMP}.sql"
+	# Clean up the previous checksum file
+	rm -vf "${PGGLOBALS}.checksum"
+	# shellcheck disable=SC2086
+	$PGDUMPALL -U $PGUSER $PGHOST -p $PGPORT "$PGDUMPALL_FLAGS" > "$PGGLOBALS"
+	RETVAL=$?
+	STATUS=("${STATUS[@]}" "$RETVAL")
+	"$MD5" $MD5FLAGS "$PGGLOBALS" >| "${PGGLOBALS}.checksum"
+fi
 
 # Take a weekly backup of each DB if needed.
 if [[ $WEEKLY == true ]] && [[ $DATESTAMP == "$WEEKLYDAY" ]]; then
 	# shellcheck disable=SC2086 
 	for DATNAME in $($PSQL -U $PGUSER template1 $PGHOST -p $PGPORT --tuples-only -c "SELECT datname FROM pg_database WHERE datistemplate IS FALSE;"); do
+		PGFILE="${BACKUPDIR}/${SERVERNAME}_${DATNAME}_${PGPORT}_${WEEKSTAMP}.sqlc"
+		# Clean up the previous checksum file
+		rm -vf "${PGFILE}.checksum"
 		# shellcheck disable=SC2086 
-		 $PGDUMP -U $PGUSER $PGHOST -p $PGPORT "$DATNAME" "$PGDUMP_FLAGS" -f "${BACKUPDIR}/${SERVERNAME}_${DATNAME}_${PGPORT}_${WEEKSTAMP}.sqlc"
-		 RETVAL=$?
-		 STATUS=("${STATUS[@]}" "$RETVAL")
+		$PGDUMP -U $PGUSER $PGHOST -p $PGPORT "$DATNAME" "$PGDUMP_FLAGS" -f "$PGFILE"
+		RETVAL=$?
+		STATUS=("${STATUS[@]}" "$RETVAL")
+		"$MD5" $MD5FLAGS "$PGFILE" >| "${PGFILE}.checksum"
 	done
+	# Back Up The Globals.
+	PGGLOBALS="${BACKUPDIR}/${SERVERNAME}_globals_${PGPORT}_${WEEKSTAMP}.sql"
+	# shellcheck disable=SC2086
+	$PGDUMPALL -U $PGUSER $PGHOST -p $PGPORT "$PGDUMPALL_FLAGS" > "$PGGLOBALS"
+	RETVAL=$?
+	STATUS=("${STATUS[@]}" "$RETVAL")
+	"$MD5" $MD5FLAGS "$PGGLOBALS" >| "${PGGLOBALS}.checksum"
 fi
 
 # We only delete if everything returned successfully.
 check_all_success "${STATUS[@]}"
 ALLOK=$?
 
+# For deletion, we actually want shell expansion, so we don't double-quote the full rm command
+
 # Delete yesterday's backups.
 if [[ $ALLOK -eq 0 ]] && [[ $ONEDAY == true ]]; then
-	rm -vf "${BACKUPDIR}/*${YESTERDAY}*"
+	# shellcheck disable=SC2086
+	rm -vf ${BACKUPDIR}/*${YESTERDAY}*
 fi
 
 # Delete weekly backups on the one-week day.
-if [[ $ALLOK -eq 0 ]] && [[ $ONEWEEK == true ]] && [[ $WEEKLYDAY == "$TODAY" ]]; then
-	rm -vf "${BACKUPDIR}/*${LASTWEEK}*"
+if [[ $ALLOK -eq 0 ]] && [[ $ONEWEEK == true ]] && [[ $WEEKLYDELETEDAY == "$TODAY" ]]; then
+	# shellcheck disable=SC2086
+	rm -vf ${BACKUPDIR}/*${LASTWEEK}*
 fi
